91adddcd6054faf7286df2545166144edc75f53c
aaanm13 <aaanm1309.1@gmail.com>
2021-07-02 01:34:12 -0300

    Aula 2.27

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Post from './components/Post';
+import Post, { PostObject } from './components/Post';
 import { useState } from 'react';
 import Card from './components/Card';
 
 function App() {
-  const [post ] = useState({
+  const [post, setPost ] = useState<PostObject|undefined>({
     title: 'Titulo qualquer',
     content: 'Lorem Ipsum Loreta Lot lau'
   })
@@ -13,16 +13,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Card title='Cartão Controle' >
-          <Post post={post}  totalComements={12}/>
-        </Card>
-        <br />
-        <Card title='Cartão Controle' 
-            align='right'
-            initializeClicked={true}>
-          <Post post={post}  totalComements={12}/>
-        </Card>
-        <br />
+        { !!post ? 
+            <>
+              <Card title='Cartão Controle' >
+                <Post post={post}  totalComements={12}/>
+              </Card>
+              <br />
+            </>
+          : <h1>404 - Post não encontrado</h1>
+        }
+        <button onClick={() => setPost(undefined)}>
+          Remover Post
+        </button>
+        { !!post ? 
+            <>
+              <Card title='Cartão Controle' 
+                  align='right'
+                  initializeClicked={true}>
+                <Post post={post}  totalComements={12}/>
+              </Card>
+              <br />
+            </>
+          : null
+        }
 
         <img src={logo} className="App-logo" alt="logo" />
         
