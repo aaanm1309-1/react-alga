0127c839e13213b37df234baee5b56bf5c3400a3
aaanm13 <aaanm1309.1@gmail.com>
2021-07-05 11:24:34 -0300

    Aula 3.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 
 function App() {
 
-  const names = ['Aluno 1','Aluno 2','Aluno 3','Aluno 4','Aluno 5']
+  const [names,setNames] = useState([
+    'Aluno 1','Aluno 2',
+    'Aluno 3','Aluno 4',
+    'Aluno 5'
+  ])
+  const [toggle,setToggle] = useState(false)
+
+  //useEffect é feito pela primeira vez quando o objeto é montado 
+  useEffect(() => {
+    console.log('Componente foi montado')
+    console.log(names)
+    // console.log(toggle)
+    // const div = document.createElement('div')
+    // div.style.height = '300px'
+    // document.body.prepend(div)
+    // document.body.append(div)
+
+  // },[names,toggle])
+
+  },[names])
+
+  useEffect(() => {
+    console.log('Componente foi montado')
+    // console.log(toggle)
+    // const div = document.createElement('div')
+    // div.style.height = '300px'
+    // document.body.prepend(div)
+    // document.body.append(div)
+
+  // },[names,toggle])
+    return () => {
+      console.log('Desmontagem do componente')
+    }
+
+  },[])
+
+  // //useEffect é feito pela primeira vez quando o objeto é montado 
+  // useEffect(() => {
+  //   console.log(toggle)
+  //   // const div = document.createElement('div')
+  //   // div.style.height = '300px'
+  //   // document.body.prepend(div)
+  //   // document.body.append(div)
+
+  // },[toggle])
 
   return (
     <div className="App">
@@ -14,6 +59,18 @@ function App() {
             (name,index) => <li key={index}> {name}</li>
           )}
         </ul>
+        <button 
+          onClick={() => 
+          setNames([...names, 'Jeremias'])}
+        >
+          Adicionar Nome
+        </button>
+        {/* <button 
+          onClick={() => 
+          setToggle(!toggle)}
+        >
+          Alterar Toggle
+        </button> */}
 
         <img src={logo} className="App-logo" alt="logo" />
         
