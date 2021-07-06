3cdcc6bcfb936de84142a48ee84ff5e77d8d6b79
aaanm13 <aaanm1309.1@gmail.com>
2021-07-06 16:10:48 -0300

    Aula 3.8

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
-import { useReducer } from 'react';
+import ScrollableBox, { ScrollableRef } from './compHook/ScrollableBox';
+import { useRef } from 'react';
 
 
-interface InitialState {
-  count: number;
-}
-
-type Action = {
-  type: 'INCREMENT',
-  payload: number 
-  } | 
-  {
-  type: |'DECREMENT'
-  }
-
-const initialState: InitialState = {
-  count: 1
-}
-
-function reducer (state: InitialState, action: Action) : InitialState {
-  console.log(action)
-
-  if (action.type === 'INCREMENT'){
-    return {
-      count: state.count + action.payload
-    }
-   
-  } else if (action.type === 'DECREMENT'){
-    return {
-      count: state.count - 1
-    }
-  }
-  return {
-    count: state.count 
-  }
-
-}
 
 
 function App() {
 
-  const [estado, dispatch] = useReducer(reducer, initialState)
+  
+  const boxRef = useRef<ScrollableRef>(null);
+
 
 
  
@@ -49,30 +17,15 @@ function App() {
     <div className="App2" >
       <header className="App-header">
 
-        <div style={{backgroundColor: 'green'}}>
-          { estado.count }
-        </div>
-
-        <button
-          onClick={() =>{
-            dispatch({
-              type: 'INCREMENT',
-              payload: 2
-            })
-          }}
-        >
-            Acrescentar
-        </button>
-        <button
-          onClick={() =>{
-            dispatch({
-              type: 'DECREMENT'
-          })
-          }}
-        >
-            Diminuir
-        </button>
-       
+        <ScrollableBox ref={boxRef} width={320} height={320} >
+           <p> 
+           Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+           There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+           </p>
+         </ScrollableBox>       
+         <button onClick={() => boxRef.current?.scrollToBottom()}>
+            descer
+         </button>
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
---
+++ src/compHook/ScrollableBox.tsx
@@ -0,0 +1,59 @@
+import { forwardRef, PropsWithChildren, useImperativeHandle, useRef } from "react";
+
+
+type ScrollableBoxProps = PropsWithChildren<{
+  width: number,
+  height: number
+}>
+
+export type ScrollableRef = {
+  scrollToBottom: () => void
+}
+
+export default forwardRef<ScrollableRef,ScrollableBoxProps>(
+  (props, ref) => {
+
+    const currentRef = useRef<HTMLDivElement>(null)
+
+    useImperativeHandle(
+      ref,
+
+      () => ({
+        scrollToBottom() {
+            if (currentRef.current){
+            
+                  currentRef.current.scrollTop = 
+                    currentRef.current.scrollHeight
+                
+            }    
+        }
+          // focus() {
+          //   window.alert('batatata')
+          //   if (currentRef.current){
+          //     currentRef.current.scrollTop = 
+          //       currentRef.current.scrollHeight
+          //   }    
+          // }
+
+      }),
+      []
+
+    )
+
+    return <div
+      ref={currentRef}
+      style={{
+      width: props.width,
+      height: props.height,
+      overflow: 'auto',
+      margin: '24px auto',
+      border: '1px solid #ccc'
+
+
+    }}
+    >
+    {props.children}
+    </div>
+
+
+  })
